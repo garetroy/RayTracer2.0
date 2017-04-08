0c1826b3bb604a2899daad0f7dc649a4952a1bd5
Garett Roberts <mygarett@gmail.com>
2017-04-07 22:11:06 -0700

    Created point

---
+++ point.h
@@ -0,0 +1,133 @@
+#ifndef _POINT_H_
+#define _POINT_H_
+
+#include <math.h>
+#include <cstdint>
+#include <iostream>
+
+using std::ostream;
+
+//#include "matrix.h"
+//#include "vector.h"
+
+template <typename T>
+struct Point{
+
+    T x, y , z;
+
+    Point(void) : x(0), y(0), z(0) {}
+    Point(const T a) : x(a), y(a), z(a) {} 
+    Point(const T x, const T y, const T z) : x(x), y(y), z(z) {}
+    Point(const Point<T>&);
+
+    Point<T>& operator=(const Point<T>);
+
+    inline Point<T>  operator*(const T) const;
+ //   inline Point<T>  operator+(const Vector<T>&) const;
+    inline Point<T>  operator-(void) const;
+//    inline Point<T>  operator-(const Vector<T>&) const;
+//    inline Vector<T> operator-(const Point<T>&) const;
+    inline T&        operator[](uint8_t i);
+           T    d_squared(const Point<T>&) const;
+           T    distance(const Point<T>&) const;
+
+    friend ostream &operator<<(ostream& os, const Point<T>& in)
+    {
+        os << "Point: (" << in.x << "," << in.y << "," << in.z << ")";
+        return os;
+    }
+};
+typedef Point<int>    Pointi;
+typedef Point<float>  Pointf;
+typedef Point<double> Pointd;
+
+template <typename T>
+Point<T>::Point(const Point<T>& p) : x(p.x), y(p.y), z(p.z)
+{} 
+
+template <typename T>
+Point<T>&
+Point<T>::operator=(const Point<T> rhs)
+{
+    if(this == &rhs)
+        return (*this);
+
+    x = rhs.x;
+    y = rhs.y;
+    z = rhs.z;
+    
+    return (*this);
+}
+
+template <typename T>
+inline Point<T>
+Point<T>::operator*(const T a) const
+{
+   return Point<T>(x*a, y*a, z*a); 
+}
+
+/*template <typename T>
+inline Point<T>
+Point<T>::operator+(const Vector<T>& in) const
+{
+    return Point<T>(x+in.x, y+in.y, z+in.z);
+}*/
+
+template <typename T>
+inline Point<T>
+Point<T>::operator-(void) const
+{
+    return Point<T>(-x,-y,-z);
+}
+
+/*template <typename T>
+inline Point<T>
+Point<T>::operator-(const Vector<T>& in) const
+{
+    return Point<T>(x-in.x, y-in.y, z-in.z);
+}
+
+template <typename T>
+inline Vector<T>
+Point<T>::operator-(const Point<T>& in) const
+{
+    return Vector<T>(x-in.x, y-in.y, z-in.z);
+}
+*/
+
+template <typename T>
+inline T&
+Point<T>::operator[](uint8_t i)
+{
+   return (&x)[i];
+}
+
+template <typename T>
+T
+Point<T>::d_squared(const Point<T>& in) const
+{
+    return (x-in.x) * (x-in.x) + (y-in.y) * (y-in.y) + (z-in.z) * (z-in.z);
+}
+
+template <typename T>
+ T
+Point<T>::distance(const Point<T>& in) const
+{
+    return sqrt(((x-in.x) * (x-in.x) + (y-in.y) * (y-in.y) + (z-in.z) * (z-in.z)));
+}
+
+template <typename T>
+inline Point<T>
+operator*(T a, const Point<T>& in)
+{
+    return Point<T>(a*in.x,a*in.y,a*in.z);
+}
+
+/*template <typename T>
+Point<T>
+operator* (const Matrix& mat, const Point<T>& in)
+{
+	return (Point<T>(mat.m[0][0] * p.x + mat.m[0][1] * p.y + mat.m[0][2] * p.z + mat.m[0][3],mat.m[1][0] * p.x + mat.m[1][1] * p.y + mat.m[1][2] * p.z + mat.m[1][3],mat.m[2][0] * p.x + mat.m[2][1] * p.y + mat.m[2][2] * p.z + mat.m[2][3]));
+}*/
+
+#endif
