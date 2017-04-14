cad01edee003600f102b052c4b6ff39b747c569b
Garett Roberts <mygarett@gmail.com>
2017-04-14 11:36:43 -0700

    Added some more modules

---
+++ include/constants.h
@@ -0,0 +1,20 @@
+#ifndef _CONSTANTS_H_
+#define _CONSTANTS_H_
+
+#include <math.h>
+#include "color.h"
+
+const double TWO_PI     = 2*M_PI;
+const double PI_ON_180  = 0.0174532925199432957; 
+const double INVPI      = 1/M_PI;
+const double INVTWO_PI  = 1/TWO_PI;
+const double kepsilon   = 0.0001;
+const double khugevalue = 1.0E10;
+
+const Colord black(0.0);
+const Colord white(1.0);
+const Colord red(1.0,0.0,0.0);
+
+const double invrandmax = 1.0 / double(RAND_MAX);
+
+#endif
---
+++ include/shaderec.h
@@ -0,0 +1,78 @@
+#ifndef _SHADEREC_H_
+#define _SHADEREC_H_
+
+#include "constants.h"
+#include "point.h"
+#include "normal.h"
+#include "color.h"
+#include "ray.h"
+
+template <typename T>
+class Material;
+template <typename T>
+class World;
+
+template <typename T>
+struct ShadeRec{
+
+    Material<T>* material;
+    Normal<T>    normal;
+    World<T>&    w;
+    Point<T>     hitpoint;
+    Point<T>     localhitpoint;
+    Color<T>     color;
+    Ray<T>       ray;
+    bool         hitobject;
+    int          depth;
+    T            t;
+    T            u;
+    T            v;
+
+    ShadeRec(World<T>&);
+    ShadeRec(const ShadeRec<T>&);
+    ~ShadeRec(void);
+};
+typedef ShadeRec<float>  ShadeRecf;
+typedef ShadeRec<double> ShadeRecd;
+
+template <typename T>
+ShadeRec<T>::ShadeRec(World<T>& in)
+    : hitobject(false),
+      material(nullptr),
+      hitpoint(),
+      normal(),
+      ray(),
+      depth(0),
+      color(black),
+      t(0.0),
+      u(0.0),
+      v(0.0),
+      w(in)
+{}
+
+template <typename T>
+ShadeRec<T>::ShadeRec(const ShadeRec& sr)
+    : hitobject(sr.hitobject),
+      material(sr.material),
+      hitpoint(sr.hitpoint),
+      localhitpoint(sr.localhitpoint),
+      normal(sr.normal),
+      ray(sr.ray),
+      depth(sr.depth),
+      color(sr.color),
+      t(sr.t),
+      u(sr.u),
+      v(sr.v),
+      w(sr.w)
+{}
+
+template <typename T>
+ShadeRec<T>::~ShadeRec(void)
+{
+    if(material){
+        delete material;
+        material = nullptr;
+    }
+}
+
+#endif
