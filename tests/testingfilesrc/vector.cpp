67d6a164658eabb227f7896b09b60cf1be00015e
Garett Roberts <mygarett@gmail.com>
2017-05-05 18:32:17 -0700

    finished vector testing

--- tests/testingfilesrc/vector.cpp
+++ tests/testingfilesrc/vector.cpp
@@ -1,5 +1,6 @@
 #include "../../include/vector.h"
 #include "../../include/point.h"
+#include "../../include/matrix.h"
 #include "../../include/normal.h"
 #include <iostream>
 
@@ -26,7 +27,7 @@ int main()
     cout << v6 << endl;
     
 
-    //-- CHecking assignment start
+    //-- Checking assignment start
     Vectord vtov = v4;
     Vectord ntov = menorm;
     Vectord ptov = mepoint;
@@ -55,12 +56,97 @@ int main()
     //Checking addition
     Vectord negthrees(-3);
     Vectord zeros(0);
-    zeros = zeros + 3;
+    zeros     = zeros + 3;
     negthrees = +negthrees;
     
     cout << negthrees << endl;
     cout << zeros << endl;
+
+    //Checking subtraction
+    Vectord negfours(-4);
+    Vectord fives(5);
+    fives    = fives + 5; 
+    negfours = -negfours;
+
+    cout << fives << endl;
+    cout << negfours << endl;
     
+
+    //Checking mulitplication
+    twos *= 0;
+    twos += 2;
+    Vectord temp(0);
+    temp = twos * 2;
+
+    cout << temp << endl;
+
+    //Checking division
+    twos *= 0;
+    twos += 2;
+    twos = twos / 2;
     
+    cout << twos << endl;
+
+    //Checking cross product 
+    Vectord in(1,2,3);
+    Vectord out(3,2,1);
+    
+    out = in^out;
+    
+    cout << out << endl;
+
+    //-- Checking assignment stop
+
+    //Checking bool operator
+    Vectord True1(1);
+    Vectord True2(1);
+    Vectord False(2); 
+
+    cout << (True1 == True2) << endl;
+    cout << (True1 == False) << endl;
+
+    //Checking [] accessor
+    Vectord testing(9,2,-4);
+   
+    cout << testing[2] << endl;
+    cout << testing[5] << endl;
+    cout << testing[-1] << endl; 
+
+    //Checking type return operations
+    Vectord sixes(6);
+    
+    cout << (sixes + sixes) << endl; 
+    cout << (sixes - sixes) << endl;
+    cout << (sixes * sixes) << endl;
+    cout << (sixes / sixes) << endl;
+
+    //Checking normalization
+    Vectord nor(9,1231,2);
+    Vectord ne(3,4,5);
+    nor = nor.hat(); 
+    ne.normalize();
+
+    cout << nor << endl;
+    cout << ne << endl;
+    
+    //Checking length
+    Vectord newtwos(2,2,2); 
+    
+    cout << newtwos.length() << endl;
+
+    //Checking left side multiplication
+    Vectord tens(10);
+    tens = 10.0 * tens;
+    
+    cout << tens << endl;
+
+    //Checking Matrix Multi
+    Vectord eleven(11);
+    Matrixd matri;
+    
+    eleven = matri * eleven;
+
+    cout << eleven << endl;
+
     return 0;
 }
