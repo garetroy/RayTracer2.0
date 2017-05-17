122dc234f55d30aa04b85447118545931f50a986
Garett Roberts <mygarett@gmail.com>
2017-05-16 19:03:33 -0700

    started testing singlesphere

--- main.cxx
+++ main.cxx
@@ -1,7 +1,22 @@
+#include <iostream>
+#include "./include/color.h"
+#include "./include/vector.h"
+#include "./include/ray.h"
+#include "./include/shaderec.h"
+#include "./include/bbox.h"
 #include "./include/point.h"
+#include "./include/mesh.h"
+#include "./include/plane.h"
+#include "./include/sphere.h"
+#include "./include/world.h"
+
+using namespace std;
 
 int 
 main()
 {
+    Worldd newworld;
+    
+    newworld.render();
     return 1;
 }
