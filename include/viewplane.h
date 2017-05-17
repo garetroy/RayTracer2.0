63412fe37ba013fd8e2e185ec34242fcfd4e595e
Garett Roberts <mygarett@gmail.com>
2017-05-16 19:02:16 -0700

    added viewplane/screen

---
+++ include/viewplane.h
@@ -0,0 +1,66 @@
+#ifndef _VIEWPLANE_H_
+#define _VIEWPLANE_H_
+
+template <typename T>
+struct ViewPlane{
+        int h;
+        int w;
+        T   s;
+        T   gamma; 
+        T   invGamma;
+        T   pixelSize;
+        
+        Color<T>* buffer;
+
+        ViewPlane(void);
+        ViewPlane(int, int); 
+        
+        void resize(int,int);
+        void setPixel(int, int, Color<T>);
+};
+
+template <typename T>
+ViewPlane<T>::ViewPlane(void)
+{
+    h = 0;
+    w = 0;
+    
+    pixelSize = 1.0;
+    gamma     = 1.0;
+
+    buffer = nullptr;
+}
+
+template <typename T>
+ViewPlane<T>::ViewPlane(int _h, int _w)
+{
+    h = _h;
+    w = _w;
+
+    pixelSize  = 1.0;
+    gamma      = 1.0; 
+    
+    buffer = new Color<T>[h*w];
+}    
+
+template <typename T>
+void
+ViewPlane<T>::resize(int _h, int _w)
+{
+    if(buffer != nullptr)
+        delete [] buffer;
+    
+    buffer = new Color<T>[h*w];
+}
+
+template <typename T>
+void
+ViewPlane<T>::setPixel(int i, int j, Color<T> in)
+{
+    if(i*j > w*h)
+        return;
+
+    buffer[i+j] = in; 
+}
+
+#endif
