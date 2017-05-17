f990b9c76325873f6be80c09c357de69dadc0da1
Garett Roberts <mygarett@gmail.com>
2017-05-16 19:02:06 -0700

    added general tracer

---
+++ include/tracer.h
@@ -0,0 +1,58 @@
+#ifndef _TRACER_H_
+#define _TRACER_H_
+
+#include "constants.h"
+#include "ray.h"
+#include "color.h"
+
+template <typename T>
+class World;
+
+template <typename T>
+class Tracer{
+    
+    public:
+        Tracer(void);
+        Tracer(World<T>* world);
+        virtual ~Tracer(void);
+    
+        virtual Color<T> traceRay(const Ray<T>& ray) const;
+        virtual Color<T> traceRay(const Ray<T>& ray,
+                                const int depth) const;
+
+    protected:
+        World<T>* world;
+};
+typedef Tracer<float>  Tracerf;
+typedef Tracer<double> Tracerd;
+
+template <typename T>
+Tracer<T>::Tracer(void) :
+    world(nullptr) {}
+
+template <typename T>
+Tracer<T>::Tracer(World<T>* world) :
+    world(world) {}
+
+template <typename T>
+Tracer<T>::~Tracer(void)
+{
+    if(world != nullptr)
+        world = nullptr;
+}
+template <typename T>
+Color<T>
+Tracer<T>::traceRay(const Ray<T>& ray) const
+{
+    return black;
+}
+
+template <typename T>
+Color<T>
+Tracer<T>::traceRay(const Ray<T>& ray, const int depth) const 
+{
+    return black;
+}
+        
+#endif
+
