cafc470db15b21f06e751f24d2883fcad3dc69bf
Garett Roberts <mygarett@gmail.com>
2017-06-10 23:33:58 -0700

    isolated bug?

--- includes/world.h
+++ includes/world.h
@@ -163,6 +163,7 @@ World<T>::hitObject(const Ray<T>& in)
     Normal<T> normal;
     Point<T>  localhitpoint;
 
+    //Problem in here?
     for (int j = 0; j < num_objects; j++)
             if (objects[j]->hit(in, t, sr) && (t < tmin)) {
                     sr.hitobject   = true;
